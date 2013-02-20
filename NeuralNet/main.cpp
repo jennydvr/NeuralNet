311a55d3e4634c6d4078bba1a86046c4657b9e45
Jenny Valdez <jedaniv@gmail.com>
2013-02-20 18:29:59 -0430

    Initial Commit

---
+++ NeuralNet/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  NeuralNet
+//
+//  Created by Jenny Valdez on 20/02/13.
+//  Copyright (c) 2013 Jenny Valdez & Luis Vieira. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
