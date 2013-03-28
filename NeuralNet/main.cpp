3ec1b9bff39b7858bda7418ee02b501946c7105e
Jenny Valdez <jedaniv@gmail.com>
2013-03-28 11:00:44 -0430

    Red funcionando

--- NeuralNet/main.cpp
+++ NeuralNet/main.cpp
@@ -6,13 +6,25 @@
 //  Copyright (c) 2013 Jenny Valdez & Luis Vieira. All rights reserved.
 //
 
-#include <iostream>
+#include "neuralNet.h"
+
+using namespace std;
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    NeuralNet mom;
+    cout << mom.toString() << endl << endl;
+    
+    vector<float> in;
+    for (int i = 0; i != 14; ++i)
+        in.push_back(i);
+    
+    vector<float> ff = mom.feedForward(in);
+    
+    for (int i = 0; i != 4; ++i) {
+        cout << ff[i] << endl;
+    }
+    
     return 0;
 }
 
