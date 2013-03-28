b96275e9c7c36b0672f319e9dbd4a65c64279797
Jenny Valdez <jedaniv@gmail.com>
2013-03-28 11:18:01 -0430

    Renombramientos

--- NeuralNet/main.cpp
+++ NeuralNet/main.cpp
@@ -1,19 +1,18 @@
 //
 //  main.cpp
-//  NeuralNet
+//  Neuralmon
 //
 //  Created by Jenny Valdez on 20/02/13.
 //  Copyright (c) 2013 Jenny Valdez & Luis Vieira. All rights reserved.
 //
 
-#include "neuralNet.h"
+#include "NeuralNet.h"
 
 using namespace std;
 
 int main(int argc, const char * argv[])
 {
     NeuralNet mom;
-    cout << mom.toString() << endl << endl;
     
     vector<float> in;
     for (int i = 0; i != 14; ++i)
